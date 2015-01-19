bf32762924bd19a2c2de5a07b20a749d291e4758
ngunner <gunnernr@gmail.com>
2015-01-18 21:01:50 -0500

    css updates

---
+++ vessel/querystrings.js
@@ -0,0 +1,27 @@
+// any element with id 'querystring' will render the current url querystring
+function querystring() {
+    document.getElementById("querystring").innerHTML =
+    window.location.search;
+}
+
+// any anchor with id 'hrefquerystring' will add the current url querystring to the end of its href attribute
+function hrefquerystring() { 
+     var anchor = document.getElementById("hrefquerystring"); 
+     anchor.href = anchor + window.location.search;
+} 
+
+// any anchor with id 'hrefquerystring2' (for 2nd instances) will add the current url querystring to the end of its href attribute
+function hrefquerystring2() { 
+     var anchor = document.getElementById("hrefquerystring2"); 
+     anchor.href = anchor + window.location.search;
+} 
+
+// run both functions in 'querystrings' function
+function querystrings() {
+  querystring();
+  hrefquerystring();
+  hrefquerystring2();
+}
+
+//run 'querystrings'
+window.onload = querystrings;
